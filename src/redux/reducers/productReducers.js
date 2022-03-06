51e5578ae1b66602a4adc8e6d603d89f5a7f1bc3
Tausif4171 <tausifkhan4171@gmail.com>
2022-03-06 11:04:37 +0530

    format document

--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -1,17 +1,19 @@
 import { ActionTypes } from "../constants/action-types";
 const initialState = {
-    products: [{
-        id:1,
-        name:"Tausif",
-        category:"Developer"
-    },],
+    products: [
+        {
+            id: 1,
+            name: "Tausif",
+            category: "Developer"
+        },
+    ],
 }
 
-export const productReducers = (state=initialState, {type, payload}) =>{
+export const productReducers = (state = initialState, { type, payload }) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
             return state;
-    
+
         default:
             return state;
     }
