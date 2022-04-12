c5ed996abb0ad93b67b05487a77e856ffa42a3b9
Tausif4171 <tausifkhan4171@gmail.com>
2022-04-13 01:58:49 +0530

    display data in our product component

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Header =()=>{
-    return(
-        <div >
+const Header = () => {
+    return (
+        <div>
             <h1>ClothesApp</h1>
         </div>
     );
--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -4,20 +4,30 @@ import { useSelector } from "react-redux";
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
     // const { id, category } = products[0];
-
-    return (
-        <div className="four column wide">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image"> </div>
-                    <div className="content">
-                        {/* <div className="header">
-                            {category}
-                        </div> */}
+    const renderList = products.map((product) => {
+        const { id, image, title, price, category } = product;
+        return (
+            <div className="four column wide" key={id}>
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image} alt={title} />
+                        </div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        );
+    })
+
+    return (
+        <>
+            {renderList}
+        </>
     );
 }
 export default ProductComponent;
