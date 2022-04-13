b7663270a17291f94a939a234bfa18463bfb6900
Tausif4171 <tausifkhan4171@gmail.com>
2022-04-13 17:33:39 +0530

    displaying product in react component from redux store

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -5,14 +5,15 @@ import { useParams } from "react-router-dom";
 import { selectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail =()=>{
-    const selectedProduct = useSelector((state)=> state.selectedProduct);
+    const product = useSelector((state)=> state.selectedProduct);
+    const {id,title,image,price,category,description} =product;
     const {productId} =useParams();
     const dispatch =useDispatch();
-    console.log(productId);
+    console.log(product);
     const fetchSelectedProductDetail =async()=>{
         const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch((err)=>{
-            console.log("Error: ", err)
-        })
+            console.log("Error: ", err);
+        });
         dispatch(selectedProduct(response.data));
     } 
     useEffect(()=>{
@@ -21,8 +22,21 @@ const ProductDetail =()=>{
         }
     },[productId]);
     return(
+        
         <div>
-            <h1>ProductDetail</h1>
+            <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image} alt={title} />
+                        </div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                            <div className="meta">{description}</div>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 }
