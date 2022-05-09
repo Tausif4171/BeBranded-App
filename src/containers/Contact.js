f3d2f0b1ab50b92209bf1817fb3c991d6af849ed
Tausif4171 <tausifkhan4171@gmail.com>
2022-05-09 23:29:11 +0530

    css added to contact page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -3,39 +3,45 @@ import { Card, Form, Button } from 'react-bootstrap'
 
 export default function Contact() {
   return (
-    <div style={{ display: "flex", justifyContent: "space-around", margin: "80px 0px 50px 0px" }}>
-      <Card style={{ width:"60rem", borderRadius: 0, margin: "0px 20px 0px 20px" }}>
+    <div className='container'>
+      <Card className='card_style'>
         <Card.Body>
-          <Card.Title>Contact Us</Card.Title>
+          <Card.Title className="contact_title">Contact Us</Card.Title>
           <div>
             <Form>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label style={{ display: "flex" }}>Name</Form.Label>
-                <Form.Control type="password" placeholder="Enter your name" />
-              </Form.Group>
+              <div style={{
+                display: "flex",
+                justifyContent: "space-between"
+              }}>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label className="contact_content">First Name</Form.Label>
+                  <Form.Control type="password" placeholder="Enter your first name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label className="contact_content">Last Name</Form.Label>
+                  <Form.Control type="password" placeholder="Enter your first name" />
+                </Form.Group>
+              </div>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label style={{ display: "flex" }}>Phone Number</Form.Label>
+                <Form.Label className="contact_content">Phone Number</Form.Label>
                 <Form.Control type="password" placeholder="Enter your phone number" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail" >
-                <Form.Label style={{ display: "flex" }}>Email address</Form.Label>
+                <Form.Label className="contact_content">Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted" style={{ display: "flex" }}>
-                  We'll never share your email with anyone else.
-                </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label style={{ display: "flex" }}>Address</Form.Label>
+                <Form.Label className="contact_content">Address</Form.Label>
                 <Form.Control as="textarea" rows={3} placeholder="Message..." />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="&nbsp;Check me out" style={{ display: "flex" }} />
+                <Form.Check type="checkbox" label="&nbsp;Check me out" className="contact_content" />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" type="submit" className='submit_button'>
                 Submit
               </Button>
             </Form>
